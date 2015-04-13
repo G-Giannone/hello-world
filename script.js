b6deb704e97715d0d6221caec85376f5489203d4
Marcelo P Farjalla <mfarjalla@gmail.com>
2015-04-12 21:03:57 -0300

    Update script.js
    Added functions to test button action in jquery.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-<!--Add your script here.-->
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').fadeTo('fast', 1);
+    });
+    $('div').mouseleave(function() {
+        $('div').fadeTo('fast', 0.5);
+        
+    });
+});
