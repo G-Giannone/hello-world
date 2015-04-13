edbb7f127801fdfe5e6d290667b6480c38b15416
Marcelo P Farjalla <mfarjalla@gmail.com>
2015-04-13 01:13:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("div").mouseenter(function() {
-        $("div").fadeTo('fast', 1);
+        $("div").fadeTo('fast', 0);
     });
     $("div").mouseleave(function() {
         $("div").fadeTo('fast', 0.5);
